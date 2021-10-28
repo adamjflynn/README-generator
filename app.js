9f1e947c9585eb56afc4401a090c3a7234c841c3
Adam Flynn <adamflynn1@gmail.com>
2021-10-27 19:54:18 -0500

    create questions to retrieve user input and promptuser function

--- app.js
+++ app.js
@@ -1,15 +1,163 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+//const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+	{
+		type: "input",
+		name: "title",
+		message: "What is the title of your project? (Required)",
+		validate: (titleInput) => {
+			if (titleInput) {
+				return true;
+			} else {
+				console.log("Please enter a title!");
+				return false;
+			}
+		},
+	},
+	{
+		type: "input",
+		name: "description",
+		message: "Please enter a description of your project. (Required)",
+		validate: (descriptionInput) => {
+			if (descriptionInput) {
+				return true;
+			} else {
+				console.log("Please enter a description of your project!");
+				return false;
+			}
+		},
+	},
+	{
+		type: "input",
+		name: "installation",
+		message:
+			"Provide a step-by-step description of how to get the development environment running. (Required)",
+		validate: (installationInput) => {
+			if (installationInput) {
+				return true;
+			} else {
+				console.log("Please provide installation instructions!");
+				return false;
+			}
+		},
+	},
+	{
+		type: "input",
+		name: "usage",
+		message: "Provide instructions and examples for use.",
+		validate: (usageInput) => {
+			if (usageInput) {
+				return true;
+			} else {
+				console.log("Please give details on usage!");
+				return false;
+			}
+		},
+	},
+	{
+		type: "list",
+		name: "license",
+		message: "Please select your license:",
+		choices: [
+			"MIT",
+			"Apache 2.0",
+			"GNU GPL v3",
+			"Mozilla Public License 2.0",
+			"No License",
+		],
+	},
+	{
+		type: "input",
+		name: "contributing",
+		message: "Enter contribution guidelines for this project",
+		validate: (contributingInput) => {
+			if (contributingInput) {
+				return true;
+			} else {
+				console.log("Please enter contribution guidelines for this project!");
+				return false;
+			}
+		},
+	},
+	{
+		type: "input",
+		name: "tests",
+		message: "Provide a way to test this application. (Required)",
+		validate: (testInput) => {
+			if (testInput) {
+				return true;
+			} else {
+				console.log("You must provide a way to test this application!");
+				return false;
+			}
+		},
+	},
+	{
+		type: "input",
+		name: "github",
+		message: "Provide a link to your GitHub repo: (Required)",
+		validate: (githubInput) => {
+			if (githubInput) {
+				return true;
+			} else {
+				console.log("You must provide a link to your GitHub repo!");
+				return false;
+			}
+		},
+	},
+	{
+		type: "input",
+		name: "email",
+		message: "Provide your email address (Required)",
+		validate: (emailInput) => {
+			if (emailInput) {
+				return true;
+			} else {
+				console.log("Please provide an email address!");
+				return false;
+			}
+		},
+	},
+	{
+		type: "input",
+		name: "github",
+		message: "Provide your GitHub Username (Required)",
+		validate: (githubInput) => {
+			if (githubInput) {
+				return true;
+			} else {
+				console.log("Please provide your GitHub Username!");
+				return false;
+			}
+		},
+	},
+	{
+		type: "confirm",
+		name: "confirmCredits",
+		message: "Do you have any credits to include?",
+		default: true,
+	},
+	{
+		type: "input",
+		name: "credits",
+		message: "List credits:",
+		when: ({ confirmCredits }) => confirmCredits,
+	},
+]);
+};
+promptUser()
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
-
-
+//init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license == "MIT") {
+        return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+} else if (license == "Apache 2.0") {
+    return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
+} else
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
