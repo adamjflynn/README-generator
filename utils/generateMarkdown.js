82d968b7138f4d86a694baa65ea0fa8aadf31fb0
Adam Flynn <adamflynn1@gmail.com>
2021-10-29 19:42:09 -0500

    add function to add the license badge when selected in the prompt

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,9 +4,15 @@ function renderLicenseBadge(license) {
     if (license == "MIT") {
         return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
 } else if (license == "Apache 2.0") {
-    return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
-} else
-
+        return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
+} else if (license == "GNU GPL v3") {
+        return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+} else if (license == "Mozilla Public License 2.0") {
+        return `![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)`
+} else if (license == "No License") {
+        return ''
+  }  
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
